e6f8f87402e9696d5e4cc1dc95e9fb59271e8307
nickdos <nickdos@gmail.com>
2015-03-03 11:50:21 +1100

    BS2 skin: JS fixes

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -16,34 +16,34 @@ modules = {
         dependsOn 'application, colorbox, fancybox, cleanHtml, snazzy, bootstrap'
         resource url:[dir:'css', file:'species.css']
         resource url:[dir:'css', file:'jquery.qtip.min.css']
-        resource url:[dir:'js', file:'jquery.sortElemets.js', disposition: 'head']
-        resource url:[dir:'js', file:'jquery.jsonp-2.3.1.min.js', disposition: 'head']
-        resource url:[dir:'js', file:'trove.js', disposition: 'head']
+        resource url:[dir:'js', file:'jquery.sortElemets.js']
+        resource url:[dir:'js', file:'jquery.jsonp-2.3.1.min.js']
+        resource url:[dir:'js', file:'trove.js']
         resource url:'https://ajax.googleapis.com/jsapi', attrs:[type:'js'], disposition: 'head'
-        resource url:[dir:'js', file:'charts2.js', disposition: 'head']
-        resource url:[dir:'js', file:'species.show.js', disposition: 'head']
-        resource url:[dir:'js', file:'audio.min.js', disposition: 'head']
-        resource url:[dir:'js', file:'jquery.qtip.min.js', disposition: 'head']
-        resource url:[dir:'js', file:'moment.min.js', disposition: 'head']
+        resource url:[dir:'js', file:'charts2.js']
+        resource url:[dir:'js', file:'species.show.js']
+        resource url:[dir:'js', file:'audio.min.js']
+        resource url:[dir:'js', file:'jquery.qtip.min.js']
+        resource url:[dir:'js', file:'moment.min.js']
     }
 
     cleanHtml {
-        resource url:[dir:'js', file:'jquery.htmlClean.js', disposition: 'head']
+        resource url:[dir:'js', file:'jquery.htmlClean.js']
     }
 
     snazzy {
-        resource url:[dir:'css', file:'snazzy.css', disposition: 'head']
+        resource url:[dir:'css', file:'snazzy.css']
     }
 
     colorbox {
         dependsOn 'jquery'
         resource url:[dir:'css', file:'colorbox.css']
-        resource url:[dir:'js', file:'jquery.colorbox-min.js'], disposition: 'head'
+        resource url:[dir:'js', file:'jquery.colorbox-min.js']
     }
 
     fancybox {
         dependsOn 'jquery'
         resource url:[dir:'css', file:'jquery.fancybox.css']
-        resource url:[dir:'js', file:'jquery.fancybox.pack.js'], disposition: 'head'
+        resource url:[dir:'js', file:'jquery.fancybox.pack.js']
     }
 }
